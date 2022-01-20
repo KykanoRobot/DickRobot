d19345a19ae8f9bd35e1c451f57b514903882181
DezKyKano <96498656+KykanoRobot@users.noreply.github.com>
2022-01-20 19:57:24 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -20,5 +20,5 @@ async def awake(event):
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/kaynorobot_bot?start=help"), Button.url("Support", "https://t.me/leave_to_me")]]
+  BUTTON = [[Button.url("Help", "https://t.me/RobotDick_bot?start=help"), Button.url("Support", "https://t.me/leave_to_me")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
